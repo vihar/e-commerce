d1ea7640e60ab81921ab4b0d373aebacb13cd05e
vihar <vihar.kurama@gmail.com>
2017-12-01 02:04:16 +0530

    Minimal Views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Categorie, Item
 
 # Create your views here.
+
+
+def home_view(self, request):
+    categories = Categorie.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'index.html', context)
+
+
+def categorie_view(self, request):
+    categories = Categorie.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'categories.html', context)
+
+
+def all_items_view(self, request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'items.html', context)
+
+
+def categorie_detail(self, request):
+    categorie = get_object_or_404(Categorie)
+    items = Item.objects.filter(categorie=categorie)
+    context = {
+        'categorie': categorie,
+        'items': items
+    }
+    return render(request, 'item_filter.html', context)
