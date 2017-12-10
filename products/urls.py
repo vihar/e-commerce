e0178ee6faf989a56070a5d0c94ae8992b0e6f42
vihar <vihar.kurama@gmail.com>
2017-12-10 14:26:44 +0530

    Templating landing page and Products View Update

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home_view, name='index'),
+    url(r'^categorie/$',
+        views.categorie_view, name='categorie'),
     url(r'^categorie/(?P<slug>[-\w]+)/$',
         views.categorie_detail, name='categorie_item_details'),
     url(r'^item/(?P<pk>[0-9]+)/$',
